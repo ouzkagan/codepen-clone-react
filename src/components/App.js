5674e4ef5a47f758597613295ab43fa125b30991
ouzKagan <oguzkaganolmez@gmail.com>
2020-09-21 03:43:44 +0300

    added styles. gave props

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React,{useState} from 'react';
+import Editor from './Editor';
 
 function App() {
+
+  const [html, setHtml] = useState('')
+  const [css, setCss] = useState('')
+  const [js, setJs] = useState('')
+  
+  
   return (
     <>
-      <div className="pane top-pane"></div>
+      <div className="pane top-pane">
+        <Editor language="xml" displayName="HTML" value={html} onChange={setHtml}/>
+        <Editor language="css" displayName="CSS" value={css} onChange={setCss}/>
+        <Editor language="javascript" displayName="Javascript" value={js} onChange={setJs}/>
+      </div>
       <div className="pane">
         <iframe
           title="output"
           sandbox="allow-scripts"
+          frameBorder="0"
+          width="100%"
+          height="100%"
         />
       </div>
     </>
